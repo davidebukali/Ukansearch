656d2086ceef70af1293468006bf8e8b0b5c2cbc
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2014-07-18 11:02:21 +0200

    Add jscs to check code formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var jshint = require('gulp-jshint');
 var gulp   = require('gulp');
-var stylish = require('jshint-stylish');
+var jscs = require('gulp-jscs');
 
 gulp.task('lint', function() {
   return gulp.src('./js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jscs());
 });
 
 // The default task (called when you run `gulp` from cli)
